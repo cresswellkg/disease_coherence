a1fc7cd49a480ab083edd9bd8f75463cb7bfcc3c
Mikhail Dozmorov <mikhail.dozmorov@gmail.com>
2019-08-25 13:41:54 -0400

    gzip BIOGRID and cleanup

--- Analysis/data/get_data.sh
+++ Analysis/data/get_data.sh
@@ -1,4 +1,5 @@
 wget https://stringdb-static.org/download/protein.links.v11.0/9606.protein.links.v11.0.txt.gz
 wget https://downloads.thebiogrid.org/Download/BioGRID/Release-Archive/BIOGRID-3.5.174/BIOGRID-ALL-3.5.174.mitab.zip
 unzip BIOGRID-ALL-3.5.174.mitab.zip
-
+gzip BIOGRID-ALL-3.5.174.mitab.txt
+rm BIOGRID-ALL-3.5.174.mitab.zip
