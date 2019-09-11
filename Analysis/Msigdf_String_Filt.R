cec025d2138cb05df16fc432b03a7f7fcddc2637
Kellen Cresswell <cresswellkg@vcu.edu>
2019-09-11 06:16:29 -0400

    new method for string filt

--- Analysis/Msigdf_String_Filt.R
+++ Analysis/Msigdf_String_Filt.R
@@ -7,6 +7,12 @@ library(biomaRt) #BiocManager::install("biomaRt")
 library(msigdbr) #BiocManager::install("msigdbr")
 library(igraph) #install.packages("igraph")
 
+#Option for number of msig pathways to take from each category
+num_msig = 10
+#Option for number of random networks to generate (Will generate num_rand-1 networks)
+num_rand = 11
+
+
 string = read_table2("./data/9606.protein.links.v11.0.txt.gz")
 
 string = string %>% mutate(protein1 = gsub("9606.", "", protein1), 
@@ -79,11 +85,15 @@ mod_over = mod_over %>% group_by(Disease) %>% mutate(Num_Genes = n())
 
 saveRDS(mod_over, "KEGG_String_Filt.rds")
 
+#Calculating normalized coherence
+
 # KEGG
 #Get msig pathway genes for KEGG, Reactome and GOCC
 msig = readRDS("KEGG_String_Filt.rds")
 #Get pathway specific counts
-Count_Sum = msig %>% dplyr::select(Disease,Category, Num_Genes) %>% distinct()
+Count_Sum = msig %>% dplyr::filter(Internal>0) %>% 
+  group_by(Disease) %>%
+  mutate(Num_Genes = n()) %>% dplyr::select(Disease,Category, Num_Genes) %>% distinct()
 #Splitting into list by categories
 Count_Sum = split(Count_Sum, Count_Sum$Category)
 coherence_tab = bind_rows()
@@ -92,27 +102,39 @@ for (i in unique(dis_slopes_string_filt_count$Disease)) {
   dis = dis_slopes_string_filt_count %>% filter(Disease == i)
   #Get 30 closest msigdf networks taking 10 from each database and bind
   close_nets = bind_rows(lapply(Count_Sum,  function(x) x %>% mutate(Diff = abs(Num_Genes-dis$Count)) %>%
-                                  arrange(Diff) %>% head(.,10)))
+                                  arrange(Diff) %>% head(.,num_msig)))
   #Subsetting msig to only include close pathways
   msig_sub = msig %>% filter(Disease %in% close_nets$Disease)
-  #Square rooting because its not already square rooted
-  mod_msig = lm(I(sqrt(External)) ~ Internal-1, data = msig_sub)
-  #Pull out coefficient
-  mod_slope = coef(mod_msig)[[1]]
-  diseases = bind_rows()
+  #Taking slopes for each category
+  mod_msig = msig_sub %>% group_by(Disease) %>% do(tidy(
+    coef(lm(I(sqrt(External)) ~ I(sqrt(Internal))-1, data = .))[[1]]
+  ))
+  #Take median
+  mod_slope = median(mod_msig$x)
+  #Creating 10 random networks
   j = 1
-  while(j<100) {
-    diseases_curr = GeneSample(dis$Count, string_filt, directed = FALSE)
-    diseases_curr = data.frame(Gene = names(diseases_curr$internal_connectivity), Internal = sqrt(diseases_curr$internal_connectivity), External = sqrt(diseases_curr$external_connectivity))
-    if (any(diseases_curr$Internal !=0)) {
-      j = j+1
+  slope_list = list()
+  while(j<num_rand) {
+    #Empty data frame for disease networks
+    diseases = bind_rows()
+    while(nrow(diseases)<dis$Count) {
+      #Generate random diseases network
+      diseases_curr = GeneSample(dis$Count, string_filt, directed = FALSE)
+      #Save internal and external edges
+      diseases_curr = data.frame(Gene = names(diseases_curr$internal_connectivity), Internal = diseases_curr$internal_connectivity, External = diseases_curr$external_connectivity)
+      #Filter out zeros
+      diseases_curr = diseases_curr %>% filter(Internal !=0)
+      #Bind together non-zero internal/external edges
       diseases = bind_rows(diseases, diseases_curr)
-      
     }
+    #Save slope of current network
+    slope_list[[j]] = coef(lm(I(sqrt(External)) ~ I(sqrt(Internal))-1, data = diseases))[[1]]
+    #Iterate
+    j = j+1
+    
   }
   
-  mod_rand = lm(External ~ Internal-1, data = diseases)
-  rand_slope = coef(mod_rand)[[1]]
+  rand_slope = median(unlist(slope_list))
   
   coherence = (dis$Slope - rand_slope)/(mod_slope-rand_slope)
   coherence = data.frame(Disease = dis$Disease, Coherence = coherence, 
@@ -121,3 +143,4 @@ for (i in unique(dis_slopes_string_filt_count$Disease)) {
   print(coherence_tab)
   saveRDS(coherence_tab, "./data/Coherence_Results/coherence_string_filt.rds")
 }
+
